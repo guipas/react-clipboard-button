0f6eb1b8fdbf5056b46fd0a39c29903d2fb29a1e
Robin Tail <robin_tail@me.com>
2024-12-30 12:16:01 +0100

    Upgrading ATTW, setting the package as ESM only.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/lib.tsx'),
+      entry: 'src/lib.tsx',
       name: 'CopyToClipboard',
       fileName: (format) => `lib.${format}.js`
     },
